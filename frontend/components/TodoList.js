57e2a07db2ea6332ca20883ace05098f9195bc4c
Sadia Gray <sadiagray17@gmail.com>
2023-11-15 23:14:55 -0600

    extracted todo data from todolist.js and added it to todo.js completed project

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
@@ -7,7 +8,11 @@ export default class TodoList extends React.Component {
     {
       this.props.todos.reduce((acc,td) => {
         if (this.props.displayComplete || !td.completed) return acc.concat(
-          <div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️' : ''}</div>
+          <Todo
+          key={this.td.todo.id}
+          toggleCompleted={this.props.toggleCompleted}
+          todo={td}
+          />
         )
         return acc
       }, [])
