e991a635b0789c01f37f1de2566cc238dfcd5c12
Sadia Gray <sadiagray17@gmail.com>
2023-11-15 23:03:45 -0600

    extracted todolist data to todolist.js from app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -67,17 +68,11 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error">Error: {this.state.error}</div>
-        <div id="todos">
-          <h2>Todos:</h2>
-          {
-            this.state.todos.reduce((acc,td) => {
-              if (this.state.displayComplete || !td.completed) return acc.concat(
-                <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✔️' : ''}</div>
-              )
-              return acc
-            }, [])
-          }
-        </div>
+        <TodoList
+        todos={this.state.todos}
+        displayComplete={this.state.displayComplete}
+        toggleCompleted={this.toggleCompleted}
+        />
        <Form>
         onTodoFormSubmit={this.onTodoFormSubmit}
         onTodoInputChange={this.onTodoInputChange}
